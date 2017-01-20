ef3357965a4fa4244d0b3ddbcd0d08abd4d23a19
Nigel Aukland <nigelaukland@gmail.com>
2017-01-20 20:14:51 +0000

    updated port

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
         // Redirect http traffic to https?
         'secureOnly':'no',
         // Set http port and https port
-        'port':'3000',
-        'secPort':'3443',
+        'port':'4000',
+        'secPort':'4wq443',
         // Set location of keys
         'key':'/bin/ca.key',
         'cert':'/bin/ca.crt'
